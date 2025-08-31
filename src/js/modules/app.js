bf2fdb9a8f7a5a979290b73ea15aa70e425dade6
SerheyBr <sergeydrilkov@mail.ru>
2025-08-31 08:30:31 +0300

    finish

--- src/js/modules/app.js
+++ src/js/modules/app.js
@@ -12,3 +12,23 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 observer.observe(trigger);
+
+// accardeon action
+const triggerAccordeonItem = document.querySelectorAll(
+  ".accordion-item__trigger"
+);
+const accardeonItems = document.querySelectorAll(".accordion-item");
+
+triggerAccordeonItem.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    if (e.currentTarget.parentElement.classList.contains("active")) {
+      e.currentTarget.parentElement.classList.remove("active");
+      return;
+    }
+    accardeonItems.forEach((el) => {
+      el.classList.remove("active");
+    });
+
+    e.currentTarget.parentElement.classList.add("active");
+  });
+});
